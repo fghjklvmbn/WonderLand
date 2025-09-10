3d43f9effd018fd98e6510442809d9adb099fd53
박경환 <66707337+fghjklvmbn@users.noreply.github.com>
2025-09-10 10:16:24 +0900

    Create backend_initDB.sql

---
+++ backend_initDB.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE wonderland DEFAULT CHARACTER SET utf8mb4;
+USE wonderland;
+
+CREATE TABLE User (
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    nickname VARCHAR(255),
+    age INT NOT NULL,
+    join_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    token_balance INT DEFAULT 10,
+    phone_number VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Story 테이블 생성 (author_id는 BIGINT로 User.user_id와 일치)
+CREATE TABLE Story (
+  story_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  author_id BIGINT NOT NULL,
+  title VARCHAR(200),
+  genre VARCHAR(50),
+  text_json JSON,
+  selected_json JSON,
+  is_draft BOOLEAN DEFAULT TRUE,
+  is_shared BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (author_id) REFERENCES User(user_id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ -- Image 테이블 생성
+CREATE TABLE Image (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    story_id BIGINT NOT NULL,
+    page_number INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (story_id) REFERENCES Story(story_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin;
